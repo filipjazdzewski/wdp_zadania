4b4234cdfc67e0c2f56648d7d21e847e1eac8ed3
filipjazdzewski <filip.r.jazdzewski@gmail.com>
2021-12-28 15:44:45 +0100

    rozwiazanie funkcji rekurencyjnej

--- lab09/zadanie8.py
+++ lab09/zadanie8.py
@@ -1,32 +1,27 @@
-def bubble_sort(list_to_sort):
-    n = len(list_to_sort)
-    j = n - 2
-    while j >= 0:
-        i = 0
-        while i <= j:
+def bubble_sort(list_to_sort):  # ładnejszy bubble sort z pętlą for
+    for j in range(len(list_to_sort) - 1):
+        for i in range(len(list_to_sort) - j - 1):
             if list_to_sort[i] > list_to_sort[i + 1]:
                 list_to_sort[i], list_to_sort[i + 1] = list_to_sort[i + 1], list_to_sort[i]
-            i += 1
-        j -= 1
     return list_to_sort
 
 
-# GÓWNO NIE DZIALA
-def binary_search(sorted_list, element, left, right):
+def binary_search(sorted_list, element, left, right):  # lista musi być posortowana, podczas wywolywania funkcji za parametr left dac liczbe 0 a za parametr right liczbe len(list) - 1
     if left < right:
-        middle = int((left+right)//2)
+        middle = (left + right) // 2
     else:
         if sorted_list[left] == element:
             return left
-        else:
-            return None
+        return None
     if sorted_list[middle] < element:
         return binary_search(sorted_list, element, middle + 1, right)
     else:
-        return binary_search(sorted_list, element, left, right)
+        return binary_search(sorted_list, element, left, middle)
 
 
 my_list = [8, 3, 28, 12, 4, -5, 10, 9, -8, 21, 5]
 my_list_sorted = bubble_sort(my_list)
-result = binary_search(my_list_sorted, 10, 0, len(my_list_sorted)-1)
+my_element = 12
+result = binary_search(my_list_sorted, my_element, 0, len(my_list_sorted)-1)
+print(my_list_sorted)
 print(result)
